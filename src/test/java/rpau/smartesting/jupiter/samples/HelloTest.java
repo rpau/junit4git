c94bc98b0278347c01615846779b3fa7ac862879
Raquel Pau <raquelpau@gmail.com>
2017-10-23 08:54:24 +0200

    jupiter test

---
+++ src/test/java/rpau/smartesting/jupiter/samples/HelloTest.java
@@ -0,0 +1,11 @@
+package rpau.smartesting.jupiter.samples;
+
+import org.junit.Test;
+
+public class HelloTest {
+
+    @Test
+    public void test() {
+        System.out.println("Jupiter executed");
+    }
+}
