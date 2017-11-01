7c0d035be38b26fbc826ea6f28434276145b3d09
Raquel Pau <raquelpau@gmail.com>
2017-11-01 16:52:49 +0100

    fixing TestIgnorer tests

--- src/test/java/org/walkmod/junit4git/core/ignorers/TestIgnorerTest.java
+++ src/test/java/org/walkmod/junit4git/core/ignorers/TestIgnorerTest.java
@@ -4,6 +4,7 @@ import org.apache.commons.io.IOUtils;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.junit.Assert;
 import org.junit.Test;
+import org.walkmod.junit4git.core.reports.AbstractReportUpdater;
 import org.walkmod.junit4git.core.reports.FileReportUpdater;
 import java.io.IOException;
 import java.io.InputStream;
@@ -15,14 +16,33 @@ import java.util.Set;
 
 public class TestIgnorerTest {
 
+    private static class MockedTestIgnorer extends TestIgnorer {
+
+        private Set<String> files;
+
+        public MockedTestIgnorer(AbstractReportUpdater updater) {
+            this(Collections.EMPTY_SET, updater);
+        }
+
+        public MockedTestIgnorer(Set<String> files, AbstractReportUpdater updater) {
+            super(updater);
+            this.files = files;
+        }
+
+        @Override
+        protected Set<String> runGitStatus() throws IOException, GitAPIException {
+            return files;
+        }
+
+        @Override
+        protected Set<String> getUpdatesFromTheBaseBranch() throws IOException, GitAPIException {
+            return new HashSet<>();
+        }
+    }
+
     @Test
     public void whenThereAreNoTestsThenNothingIsIgnored() throws Exception {
-        TestIgnorer resolver = new TestIgnorer(new FileReportUpdater()){
-            @Override
-            protected Set<String> runGitStatus() throws IOException, GitAPIException {
-                return Collections.EMPTY_SET;
-            }
-        };
+        TestIgnorer resolver = new MockedTestIgnorer(new FileReportUpdater());
         Set<String> result = resolver.getTestsToIgnore(IOUtils.toInputStream("[]", Charset.forName("UTF-8")));
         Assert.assertEquals(Collections.EMPTY_SET, result);
     }
@@ -43,12 +63,7 @@ public class TestIgnorerTest {
             }
         };
 
-        TestIgnorer resolver = new TestIgnorer(updater){
-            @Override
-            protected Set<String> runGitStatus() throws IOException, GitAPIException {
-                return Collections.EMPTY_SET;
-            }
-        };
+        TestIgnorer resolver = new MockedTestIgnorer(updater);
         Set<String> result = resolver.getTestsToIgnore(updater.getBaseReport());
         Set<String> expected = new HashSet<>(Arrays.asList("MyTest#testMethod"));
         Assert.assertEquals(expected, result);
@@ -56,12 +71,9 @@ public class TestIgnorerTest {
 
     @Test
     public void whenThereIsATestWhichIsModifiedThenIsNotIgnored() throws Exception {
-        TestIgnorer resolver = new TestIgnorer(new FileReportUpdater()){
-            @Override
-            protected Set<String> runGitStatus() throws IOException, GitAPIException {
-                return new HashSet<>(Arrays.asList("MyTest.java"));
-            }
-        };
+        TestIgnorer resolver = new MockedTestIgnorer(new HashSet<>(Arrays.asList("MyTest.java")),
+                new FileReportUpdater());
+
         Set<String> result = resolver.getTestsToIgnore(IOUtils.toInputStream(
                 "[{\n" +
                         "  \"test\": \"MyTest\",\n" +
@@ -76,12 +88,10 @@ public class TestIgnorerTest {
 
     @Test
     public void whenThereIsARefClassWhichIsModifiedThenIsNotIgnored() throws Exception {
-        TestIgnorer resolver = new TestIgnorer(new FileReportUpdater()){
-            @Override
-            protected Set<String> runGitStatus() throws IOException, GitAPIException {
-                return new HashSet<>(Arrays.asList("org/walkmod/junit4git/samples/Hello.java"));
-            }
-        };
+        TestIgnorer resolver = new MockedTestIgnorer(
+                new HashSet<>(Arrays.asList("org/walkmod/junit4git/samples/Hello.java")),
+                new FileReportUpdater());
+
         Set<String> result = resolver.getTestsToIgnore(IOUtils.toInputStream(
                 "[{\n" +
                         "  \"test\": \"MyTest\",\n" +
@@ -96,12 +106,9 @@ public class TestIgnorerTest {
 
     @Test
     public void whenThereAreNestedClassesThenTheParentClassIsUsed() throws Exception {
-        TestIgnorer resolver = new TestIgnorer(new FileReportUpdater()){
-            @Override
-            protected Set<String> runGitStatus() throws IOException, GitAPIException {
-                return new HashSet<>(Arrays.asList("org/walkmod/junit4git/samples/Hello.java"));
-            }
-        };
+        TestIgnorer resolver = new MockedTestIgnorer(
+                new HashSet<>(Arrays.asList("org/walkmod/junit4git/samples/Hello.java")),
+                new FileReportUpdater());
         Set<String> result = resolver.getTestsToIgnore(IOUtils.toInputStream(
                 "[{\n" +
                         "  \"test\": \"MyTest\",\n" +
@@ -116,13 +123,9 @@ public class TestIgnorerTest {
 
     @Test
     public void whenThereAreClassesWithSameSuffixCorrectOnesAreIgnored() throws Exception {
-        TestIgnorer resolver = new TestIgnorer(new FileReportUpdater()){
-            @Override
-            protected Set<String> runGitStatus() throws IOException, GitAPIException {
-                return new HashSet<>(Arrays.asList(
-                        "org/walkmod/junit4git/samples/Hello.java"));
-            }
-        };
+        TestIgnorer resolver = new MockedTestIgnorer(new HashSet<>(Arrays.asList(
+                "org/walkmod/junit4git/samples/Hello.java")), new FileReportUpdater());
+
         Set<String> result = resolver.getTestsToIgnore(IOUtils.toInputStream(
                 "[{\n" +
                         "  \"test\": \"MyTest\",\n" +
